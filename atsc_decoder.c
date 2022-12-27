b7db0154232b704fa79aded3db7db3f72ea6cfb0
Fukai The Demon King <59860555+DemonKingFukai@users.noreply.github.com>
2022-12-27 02:11:27 -0800

    Update atsc_decoder.c

--- atsc_decoder.c
+++ atsc_decoder.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <string.h>
 #include <portapack.h>
 #include <atsc.h>
 
@@ -16,6 +16,29 @@
 #include <atsc_channels.h>
 
 #define CHANNEL_COUNT sizeof(atsc_channels) / sizeof(atsc_channel_t)
+void setup() {
+// Initialize the Portapack and ATSC library
+portapack_init();
+atsc_init();
+
+// Set the frequency to the desired ATSC channel
+atsc_set_frequency(ATSC_CHANNEL_7);
+
+// Start the ATSC receiver
+atsc_start();
+}
+
+void loop() {
+// Check if there is a new ATSC packet available
+if (atsc_packet_available()) {
+// Get the current ATSC packet
+atsc_packet_t packet = atsc_get_packet();
+
+// Display the channel information on the screen
+portapack_lcd_printf(0, 0, "Channel: %d", packet.channel);
+portapack_lcd_printf(0, 1, "Program: %d", packet.program_number);
+portapack_lcd_printf(0, 2, "Program Name: %s", packet.program_name);
+
 
 static void vTaskATSC( void *pvParameters ) {
 	static atsc_rx_t atsc_rx;
